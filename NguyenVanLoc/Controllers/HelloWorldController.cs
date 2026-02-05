dc43fffc65a06effaf70646bc9e9748163058394
lyach <lyach@ENVIEO>
2026-02-05 12:55:29 +0700

    Add project files.

---
+++ NguyenVanLoc/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using System.Xml.Linq;
+
+namespace NguyenVanLoc.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        
+            
+            public string Index()
+            {
+                return "Hello Cô Phương...";
+            }
+            
+
+            public string Welcome()
+            {
+                return "Welcome to website nvl";
+            }
+
+            public string Welcome1(string name, string ID="Nguyen Van Loc")
+            {
+            return HtmlEncoder.Default.Encode($"Hello {name}, My name is: {ID}");
+            }
+    }
+    }
+
---
+++ NguyenVanLoc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NguyenVanLoc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
